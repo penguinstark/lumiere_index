dcb4cfa38bb459681dbbdda9338ca07df70f34a3
Kaushik Karthik Prasanna <kaushikkarthikp@gmail.com>
2022-04-06 05:10:34 -0400

    data now stored as index number, title, and link in JSON

--- lumScraper.py
+++ lumScraper.py
@@ -1,9 +1,12 @@
-import requests
+import requests, json
 from bs4 import BeautifulSoup
-for i in range(49290,49390):
-    URL = "https://lumiere.berkeley.edu/students/items/"+str(i)
-    page = requests.get(URL)
+
+f = open('/Users/kaushik/Documents/lumiere_index/lumIndices.json', 'w')
+
+for i in range(49300,49400):
+    page = requests.get("https://lumiere.berkeley.edu/students/items/"+str(i))
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(class_="page-title column span-24")
     if results.text.strip():
-        print(str(i)+results.text)
+        data = [str(i), results.text.strip(),"https://lumiere.berkeley.edu/students/items/"+str(i)]
+        json.dump(data, f)
